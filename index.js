61c40925c4c435064099bc83d87b6562a60a2437
Abiy Seifu <abiy@ripple.com>
2014-09-04 18:37:11 -0700

    [TASK] remove ripple folder

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ exports.config = require(__dirname + '/config/config.js');
 exports.data   = require(__dirname +'/lib/data/');
 exports.api = requireAll(__dirname+'/lib/api/');
 exports.errors = requireAll(__dirname+'/lib/errors/');
-exports.ripple = require(__dirname +'/lib/ripple/');
 exports.validator = require(__dirname+'/lib/validator.js');
 exports.server = require(__dirname+'/lib/app.js');
 
