696d9e79b447156f0f47dc5092f8b7232ad880f6
Steven Zeiler <zeiler.steven@gmail.com>
2014-09-07 22:04:29 -0700

    [TASK] Remove unused Ripple, move config.js config/config.js now belongs in lib/config.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var requireAll = require('require-all-to-camel');
 
-exports.config = require(__dirname + '/config/config.js');
+exports.config = require(__dirname + '/lib/config.js');
 exports.data  = require(__dirname +'/lib/data/');
 exports.models = requireAll(__dirname +'/lib/data/models/');
 exports.api = requireAll(__dirname+'/lib/api/');
 exports.errors = requireAll(__dirname+'/lib/errors/');
-exports.ripple = require(__dirname +'/lib/ripple/');
 exports.validator = require(__dirname+'/lib/validator.js');
 exports.server = require(__dirname+'/lib/app.js');
 exports.processes = require(__dirname+'/lib/processes/');
